bdcea35a6ab3e739ebcb86ca7c0086be31d8d0ef
Michael Lehmann <lehmamic@bluewin.ch>
2017-09-15 15:02:02 +0200

    remove demo output from previous example

--- index.ts
+++ index.ts
@@ -16,25 +16,6 @@ let reducer: Reducer<number> = (state: number, action: Action) => {
     }
 };
 
-let incrementAction: Action = {
-    type: 'INCREMENT'
-}
-
-console.log(reducer(0, incrementAction)); // => 1
-console.log(reducer(1, incrementAction)); // => 2
-
-let decrementAction: Action = {
-    type: 'DECREMENT'
-}
-
-console.log(reducer(100, decrementAction)); // => 99
-
-let unknownAction: Action = {
-    type: 'UNKNOWN'
-};
-
-console.log(reducer(100, unknownAction)); // => 100
-
 console.log(reducer(3, { type: 'PLUS', payload: 7 })); // => 10
 console.log(reducer(3, { type: 'PLUS', payload: 9000 })); // => 9003
 console.log(reducer(3, { type: 'PLUS', payload: -2 })); // => 1
