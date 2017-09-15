21b7210860c4bde2775f55a416a5942925287ffe
Michael Lehmann <lehmamic@bluewin.ch>
2017-09-15 14:31:47 +0200

    reformat and demo an unknown action

--- index.ts
+++ index.ts
@@ -1,13 +1,5 @@
 import { Action, Reducer } from './redux';
 
-let incrementAction: Action = {
-    type: 'INCREMENT'
-}
-
-let decrementAction: Action = {
-    type: 'DECREMENT'
-}
-
 let reducer: Reducer<number> = (state: number, action: Action) => {
     switch(action.type) {
         case 'INCREMENT':
@@ -17,11 +9,25 @@ let reducer: Reducer<number> = (state: number, action: Action) => {
             return state - 1;
 
         default:
-            return state;
+            return state; // <-- don't forget!
     }
 };
+
+let incrementAction: Action = {
+    type: 'INCREMENT'
+}
 
 console.log(reducer(0, incrementAction)); // => 1
 console.log(reducer(1, incrementAction)); // => 2
 
+let decrementAction: Action = {
+    type: 'DECREMENT'
+}
+
 console.log(reducer(100, decrementAction)); // => 99
+
+let unknownAction: Action = {
+    type: 'UNKNOWN'
+};
+
+console.log(reducer(100, unknownAction)); // => 100
