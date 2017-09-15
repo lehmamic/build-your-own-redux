c7e7fa2d957be0dc718f0d09e6b4896b76e4fad9
Michael Lehmann <lehmamic@bluewin.ch>
2017-09-15 14:28:38 +0200

    introduce counter actions

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
 import { Action, Reducer } from './redux';
 
+let incrementAction: Action = {
+    type: 'INCREMENT'
+}
+
+let decrementAction: Action = {
+    type: 'DECREMENT'
+}
+
 let reducer: Reducer<number> = (state: number, action: Action) => {
-    return state;
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1;
+
+        case 'DECREMENT':
+            return state - 1;
+
+        default:
+            return state;
+    }
 };
 
-console.log(reducer(0, null)); // => 0
+console.log(reducer(0, incrementAction)); // => 1
+console.log(reducer(1, incrementAction)); // => 2
+
+console.log(reducer(100, decrementAction)); // => 99
